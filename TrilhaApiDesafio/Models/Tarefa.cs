1fba330817d8be008f68fd7817399c7fcd2eeb72
André Nunes de Lima <andre.nunes@live.com>
2025-08-07 22:49:43 -0300

    fix: Add Annotation to Tarefa Model

--- TrilhaApiDesafio/Models/Tarefa.cs
+++ TrilhaApiDesafio/Models/Tarefa.cs
@@ -1,10 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace TrilhaApiDesafio.Models;
 
 public class Tarefa
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
-    public string Titulo { get; set; }
-    public string Descricao { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    public string Titulo { get; set; } = null!;
+    
+    [StringLength(500)]
+    public string? Descricao { get; set; }
+
+    [Required]
     public DateTime Data { get; set; }
+
+    [Required]
     public EnumStatusTarefa Status { get; set; }
 }
